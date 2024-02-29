6d5f49133b5f7696ac70112ecca2a2387c0515d4
YamakenElf <810658920qq@gmail.com>
2024-02-29 19:44:12 +0800

    a test for libtorch work with ros

--- src/test_torch.cpp
+++ src/test_torch.cpp
@@ -6,23 +6,23 @@ int main(int argc, char **argv){
     ros::Rate rate(1);
 
     // Load the PyTorch model
-    std::string model_path = "/home/wx/catkin_ws/src/quarotor_feedback_controller/Model/Hover_target/PPO66.pt";
+    std::string model_path = "/home/wx/catkin_ws/src/test_torch/Model/test_model.pt";
     torch::jit::script::Module RL_model = torch::jit::load(model_path);
     std::cout << "RL module Loaded!\n" << std::endl;
 
-    Eigen::VectorXd obs_(10);
-    Eigen::VectorXf float_obs_(10);
+    Eigen::VectorXd obs(10);    // double type
+    Eigen::VectorXf float_obs(10);  // double type
 
     while (ros::ok()){
         obs.setRandom();
-        float_obs = obs.cast<float>();
+        float_obs = obs.cast<float>();  // float type
         std::cout << "Input vector: " << float_obs << std::endl;
 
         torch::Tensor input_tensor = torch::from_blob(float_obs.data(), {1, 10}, torch::kFloat32).clone();
 
         std::cout << "Input tensor: " << input_tensor << std::endl;
 
-        // Run inference
+        // Run inference (model needs float type)
         torch::jit::IValue outputs = RL_model.forward({input_tensor});
 
         // Extract output tensor
